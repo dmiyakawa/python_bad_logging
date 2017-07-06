530ead37146b2700e95fdbff43ee53967a43219c
Daisuke Miyakawa <d.miyakawa+mac@gmail.com>
2017-07-07 08:25:29 +0900

    より意図が分かりやすいようにシンプルに

--- dmiyakawabadlogging/__init__.py
+++ dmiyakawabadlogging/__init__.py
@@ -4,12 +4,11 @@ import time
 logger = logging.getLogger()
 
 def mygoodfunction():
-    if logger.getEffectiveLevel() <= logging.INFO:
+    if logger.getEffectiveLevel() < logging.INFO:
         logging.debug('Start very timeconsuming debug setup!!')
         time.sleep(2)
         logging.debug('Yahoo!!!')
         time.sleep(2)
         logging.debug('Google!!!')
         time.sleep(2)
-    print('This is good.')
     return 'Good Value'
